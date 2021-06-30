960f5176974b39b3d7e8871073f90e8b5d934a02
smit1298 <61621188+smit1298@users.noreply.github.com>
2021-06-30 09:22:51 +0100

    second

--- .history/routes/users_20210629194919.js
+++ .history/routes/users_20210629194919.js
@@ -3,7 +3,7 @@ const router  = express.Router();
 
 
 // @route POST api/users
-// @desc Register a user
+// @desc Register a users
 // @access Public
 router.post('/', (req, res) => {
     res.send('Register a user')
